e5d3831d5f92641eb8c8be995e886f3578ee9d12
Apophis_ <ancientphoebus@gmail.com>
2021-06-21 21:16:08 +0530

    mild changes to layout of list card

--- lib/widgets/transactionListCard.dart
+++ lib/widgets/transactionListCard.dart
@@ -4,51 +4,67 @@ import 'package:intl/intl.dart';
 
 class TransactionListCard extends StatelessWidget {
   final List<Transaction> _transactions;
-
-  TransactionListCard(this._transactions);
+  final Function removeTransaction;
+  TransactionListCard(this._transactions,this.removeTransaction);
 
   Widget build(BuildContext context) {
     return Container(
-      height: 400,
-      child: ListView.builder(
-        itemCount: _transactions.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Card(
-            child: Row(children: <Widget>[
-              Container(
-                padding: EdgeInsets.all(10),
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                child: Text(
-                  // "Rs" + transactionObject.amount.toString(), this is concatenation, use interpolation
-                  "Rs ${_transactions[index].amount.toStringAsFixed(2)}", //this is interpolation dollar sign used for concatenation
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.purple,
-                  ),
-                ),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    style: BorderStyle.solid,
-                    color: Colors.purple,
-                    width: 2,
-                  ),
-                ),
-              ),
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: <
-                  Widget>[
-                Text(
-                  _transactions[index].title,
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
-                ),
-                Text(
-                    DateFormat("MMM d, yyyy").format(_transactions[index].date),
-                    style: TextStyle(color: Colors.grey)),
-              ]),
-            ]),
-          );
-        },
-      ),
+      height: 380,
+      child: _transactions.isEmpty
+          ? Container(
+              margin: EdgeInsets.only(top: 100),
+              child: Image.asset("assets/images/stock.png", fit: BoxFit.cover),
+            )
+          : ListView.builder(
+              itemCount: _transactions.length,
+              itemBuilder: (BuildContext context, int index) {
+                print(index);
+                return Card(
+                  elevation:4,
+                  margin:EdgeInsets.symmetric(vertical:3,horizontal:5),
+                  child: Row(children: <Widget>[
+                    Container(
+                      width:100,
+                      padding: EdgeInsets.all(10),
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                      child: FittedBox(
+                        child: Text(
+                          // "Rs" + transactionObject.amount.toString(), this is concatenation, use interpolation
+                          "Rs ${_transactions[index].amount.toStringAsFixed(2)}", //this is interpolation dollar sign used for concatenation
+                          style: Theme.of(context).textTheme.headline6,
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          style: BorderStyle.solid,
+                          color: Colors.purple,
+                          width: 2,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              _transactions[index].title,
+                              style: Theme.of(context).textTheme.headline6,
+                            ),
+                            Text(
+                                DateFormat("MMM d, yyyy")
+                                    .format(_transactions[index].date),
+                                style: TextStyle(color: Colors.grey)),
+                          ]),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: () {removeTransaction(_transactions[index].id);},
+                    )
+                  ]),
+                );
+              },
+            ),
     );
   }
 }
