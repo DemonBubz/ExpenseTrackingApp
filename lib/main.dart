8e8e9cc5c2f6ecb083972449cef17d8da56d0e1c
Apophis_ <ancientphoebus@gmail.com>
2021-06-21 21:12:00 +0530

    add theme, converted to stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import './widgets/transactionListCard.dart';
 import 'package:flutter/material.dart';
-import 'widgets/transactionDisplay.dart';
+import 'widgets/textFieldCard.dart';
+import 'models/transaction.dart';
+import 'widgets/chartWidget.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,24 +12,83 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: MyHomePage(),
+      title: "My expense Tracker",
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        accentColor: Colors.greenAccent,
+        scaffoldBackgroundColor: Colors.white,
+        textTheme: ThemeData.light().textTheme.copyWith(
+              headline6: TextStyle(
+                fontWeight: FontWeight.w400,
+                fontSize: 19,
+                fontFamily: "Lato",
+              ),
+              button: TextStyle(color: Colors.white),
+            ),
+        appBarTheme: AppBarTheme(
+          textTheme: ThemeData.light().textTheme.copyWith(
+                headline6: TextStyle(
+                    fontFamily: "Nunito",
+                    fontWeight: FontWeight.w700,
+                    fontSize: 22),
+              ),
+        ),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  State<StatefulWidget> createState() {
+    return MyHomePageState();
+  }
+}
+
+class MyHomePageState extends State<MyHomePage> {
   // String titleInput="";
   // String amountInput="";
-
   //alternative
+  final List<Transaction> _transactions = [];
 
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
+  void addTransaction({title, amount, date}) {
+    setState(() {
+      _transactions.add(new Transaction(
+          amount: amount,
+          title: title,
+          id: DateTime.now().millisecond,
+          date: date));
+    });
+  }
+
+  void removeTransaction(int id) {
+    setState(() {
+      _transactions.removeWhere((items) {
+        return items.id == id;
+      });
+    });
+  }
+
+  void bringSheetUp(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (BuildContext bContext) {
+          return TextFieldCard(addTransaction);
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Expense Tracker"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              bringSheetUp(context);
+            },
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -34,16 +96,24 @@ class MyHomePage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Card(
+              margin: EdgeInsets.all(4),
               child: Container(
-                child: Text("Our Chart will later go here"),
+                child: ChartWidget(_transactions),
                 width: double.infinity,
               ),
               elevation: 5,
-              color: Colors.blue,
+              color: Theme.of(context).primaryColor,
             ),
-            TransactionDisplay(),
+            TransactionListCard(_transactions,removeTransaction),
           ],
         ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          bringSheetUp(context);
+        },
       ),
     );
   }
